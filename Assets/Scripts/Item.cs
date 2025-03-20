576fb0666d2f25a32d1d58dbd9dbff2ee19befa4
Mystery_Z <1207244429@qq.com>
2025-03-20 17:40:55 +0800

    Item类添加时停逻辑相关

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,20 +1,46 @@
 using Newtonsoft.Json.Serialization;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class Item:MonoBehaviour
 {
-    [SerializeField] private float yOffset = 5f;
-    [SerializeField] private float forceMagnitude;
+    [Header("重力操控相关")]
     [SerializeField] private float launchAngle = 45f;
-
     private Rigidbody rb;
     private bool isGravityChanged = false;
+
+
+    [Header("时停操控相关")]
+    [SerializeField] private Material timeStoppedMaterial;
+    private Material originMaterial;
+    private float timer;
+    private bool isTimeStopped;
+
+    //定义时停结束事件和委托
+    public delegate void OnTimeStoppedCanceled();
+    public static event OnTimeStoppedCanceled onTimeStoppedCanceled;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        originMaterial = GetComponent<Renderer>().material;
     }
 
+    private void Update()
+    {
+        if (isTimeStopped)
+        {
+            //当时停时，时停计数器开始计数
+           timer -= Time.deltaTime;
+            if(timer < 0)
+            {
+                //计数时间到时，解除物体时停状态
+                CancelTimeStop();
+                isTimeStopped = false;
+            }
+        }
+    }
     private void FixedUpdate()
     {
         if (isGravityChanged)
@@ -24,6 +50,7 @@ public class Item:MonoBehaviour
             rb.AddForce(reverseGravity, ForceMode.Force);
         }
     }
+
 
     public void ChangeGravity()
     {
@@ -77,9 +104,55 @@ public class Item:MonoBehaviour
         Vector3 launchVelocity = velocityXZ + Vector3.up * velocityY;
         return launchVelocity;
     }
-
-    public void TimeStop()
+    private void SetTimeStopTimer(float time)
     {
+        timer = time;
+    }
 
+    public bool IsTimeStopped()
+    {
+        return isTimeStopped;
+    }
+    
+    public void TimeStop(float time)
+    {
+        //改变材质
+        ChangeMaterial(timeStoppedMaterial);
+        //设置计数器，同时将物体置为已时停状态
+        SetTimeStopTimer(time);
+        isTimeStopped = true;
+
+        //禁用物体物理效果
+        rb.isKinematic = true;
+    }
+
+    public void CancelTimeStop()
+    {
+        //重置物体材质
+        ResetMaterial();
+        isTimeStopped = false;
+
+        /*当事件不为空时，响应函数调用
+         * (即在BaseTimeGunMode里订阅了onTimeStoppedCanceled事件，当事件不为空时，调用订阅了这个事件的函数，即PlusTimeStopCount())
+         * "?."等效传统写法：
+         * if (onTimeStoppedCanceled != null) 
+         * {
+              onTimeStoppedCanceled.Invoke();
+           }
+         */
+        onTimeStoppedCanceled?.Invoke();
+
+        //重置物体物理效果
+        rb.isKinematic = false;
+    }
+
+    public void ChangeMaterial(Material material)
+    {
+        transform.GetComponent<Renderer>().material = material;
+    }
+
+    public void ResetMaterial()
+    {
+        transform.GetComponent<Renderer>().material = originMaterial;
     }
 }
