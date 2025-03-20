8344d1173c6660f101cfa4d919584e4b30f1b0a2
Mystery_Z <1207244429@qq.com>
2025-03-20 19:17:54 +0800

    移除GunMode右键相关逻辑

--- Assets/Scripts/GunMode.cs
+++ Assets/Scripts/GunMode.cs
@@ -1,18 +1,21 @@
+using System;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class GunMode : MonoBehaviour
 {
     [SerializeField] protected Transform fpsCam;
     [SerializeField] protected float interactionRange;
 
+
     protected virtual void HandlerInteraction() { }
-    protected virtual void OnUpdate() { }
+    protected virtual void OnUpdate()
+    {
+    }
 
     protected virtual void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse1))
-        {
-            HandlerInteraction();
-        }
+        HandlerInteraction();
     }
+
 }
