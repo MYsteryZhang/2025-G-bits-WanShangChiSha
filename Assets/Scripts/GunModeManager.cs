4f14a89ddf8b3986b42aa180d9a1b1f241e9eae7
Mystery_Z <1207244429@qq.com>
2025-03-24 17:21:49 +0800

    更改切换模式逻辑，按住右键切换到时停枪模式，松开右键切回重力枪模式，同时切换模式时切换枪的模型

--- Assets/Scripts/GunModeManager.cs
+++ Assets/Scripts/GunModeManager.cs
@@ -7,10 +7,16 @@ public class GunModeManager : MonoBehaviour
     public static GunModeManager Instance { get; private set; }
 
     [SerializeField] private GameObject Gun;
+    [SerializeField] private GameObject GravityGunPrefab;
+    [SerializeField] private GameObject TimeGunPrefab;
+
     private BaseGravityGunMode _currentGravityGunModeScript;
     private BaseTimeGunMode _currentTimeGunModeScript;
 
     private bool isGravityModeActive = true;
+
+    public delegate void OnGunModeChanged();
+    public static event OnGunModeChanged onGunModeChange;
 
     //确保Manager类只有一个实例，全局可访问
     private void Awake()
@@ -28,6 +34,8 @@ public class GunModeManager : MonoBehaviour
     private void Start()
     {
         SetGunMode(isGravityModeActive);
+        GravityGunPrefab.SetActive(true);
+        TimeGunPrefab.SetActive(false);
     }
 
     private void SetGunMode(bool isGravityMode)
@@ -45,24 +53,39 @@ public class GunModeManager : MonoBehaviour
 
     private void Update()
     {
-        //当玩家按下G时，禁用当前模式脚本，启用另一模式脚本
-        if(Input.GetKeyDown(KeyCode.G))
+        //当玩家按下右键时，禁用当前模式脚本，启用另一模式脚本
+        if(Input.GetKeyDown(KeyCode.Mouse1))
         {
             //只有当且仅当枪械同时拥有两个模式时才能进行模式切换
             if(HasGravityAndTimeMode())
             {
-                isGravityModeActive = !isGravityModeActive;
-                SetGunMode(isGravityModeActive);
+                ChangeGunMode();
             }
         }
+        //当松开右键时切换回原来的模式
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            if (HasGravityAndTimeMode())
+            {
+                ChangeGunMode();
+            }
+        }
+    }
+
+    private void ChangeGunMode()
+    {
+        isGravityModeActive = !isGravityModeActive;
+        SetGunMode(isGravityModeActive);
+        SwitchGunPrefab(isGravityModeActive);
     }
 
     private bool HasGravityAndTimeMode()
     {
         return Gun.transform.GetComponent<BaseGravityGunMode>() != null && Gun.transform.GetComponent<BaseTimeGunMode>() != null;
     }
-    public void ChangeGunMode()
+    
+    private void SwitchGunPrefab(bool _isGravityModeActive)
     {
-        
+        TimeGunPrefab.SetActive(!_isGravityModeActive);
     }
 }
