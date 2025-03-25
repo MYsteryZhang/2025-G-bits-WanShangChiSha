ac7da554c48fb25e9fd7adeefd4dfe9efb8e5ae3
Mystery_Z <1207244429@qq.com>
2025-03-25 16:16:12 +0800

    增加力的箭头的指示逻辑，使其始终指向最终物体受力方向

--- Assets/Scripts/Level2GravityGunMode.cs
+++ Assets/Scripts/Level2GravityGunMode.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.ProBuilder;
+using UnityEngine.Rendering;
 
 public class Level2GravityGunMode : BaseGravityGunMode
 {
     [SerializeField] private float pushForce = 10f;
+    [SerializeField] private float offsetFactor = 1.2f;//位移参数，控制位移偏量
+    [SerializeField] private int maxPushCounts = 7;
+    [SerializeField] private GameObject forceArrowPrefab;
     [SerializeField] GameObject player;
 
-
+    private Vector3 finalForce = Vector3.zero;
+    private GameObject forceArrow;
     private void Start()
     {
         //二段跳逻辑放在PlayerMovement中，当二级重力枪解锁的时候，解锁二段跳技能
@@ -30,7 +36,7 @@ public class Level2GravityGunMode : BaseGravityGunMode
                 {
 
                     //当物品在时停状态的时候，累积动量，当时停结束的时候，执行AddForce
-                    if (hit.transform.GetComponent<Item>().IsTimeStopped())
+                    if (hit.transform.GetComponent<Item>().IsTimeStopped() && maxPushCounts > 0)
                     {
                         StartCoroutine(PushItemWhenTimeStopCanceled(hit));
                     }
@@ -55,15 +61,39 @@ public class Level2GravityGunMode : BaseGravityGunMode
 
     protected IEnumerator PushItemWhenTimeStopCanceled(RaycastHit hit)
     {
+        //计算玩家对物体施加推力的方向
         Vector3 direction = hit.transform.position - fpsCam.transform.position;
+        //标准化力的方向，使其仅影响方向而不影响力的大小
         direction = Vector3.Normalize(direction);
+        //累积最后对物体施加的最终的力
+        finalForce += direction * pushForce;
+        maxPushCounts--;
+
+        if (forceArrow == null)
+        {
+            //finalForce.normalized * pushForce / offsetFactor使每次叠加更大的力时，使箭头向力的方向进行位移偏量，避免箭头穿模物体
+            forceArrow = Instantiate(forceArrowPrefab, hit.transform.position + finalForce / offsetFactor, Quaternion.identity);
+        }
+        else 
+        {
+            //每次施加力都重置力的指示方向
+            Destroy(forceArrow);
+            forceArrow = Instantiate(forceArrowPrefab, hit.transform.position + finalForce / offsetFactor, Quaternion.identity);
+        }
+
+        forceArrow.GetComponent<ForceArrow>().Initialize(finalForce);
+
         //当物体还在时停状态的时候通过yield return null暂停协程
         while (hit.transform.GetComponent<Item>().IsTimeStopped())
         {
             yield return null;
         }
-        //当物体退出时停状态后，执行后面的逻辑
+
+        //当物体退出时停状态后，执行Push，重置finalForce为零
         PushItem(hit, direction);
+        finalForce = Vector3.zero;
+        maxPushCounts++;
+        Destroy(forceArrow);
     }
 
     protected override void Update()
