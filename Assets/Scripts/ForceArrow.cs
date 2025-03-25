714a1da0b902bca08bfa2f30efee9d141702ff2a
Mystery_Z <1207244429@qq.com>
2025-03-25 16:15:19 +0800

    为箭头增加控制其初始化的脚本

---
+++ Assets/Scripts/ForceArrow.cs
@@ -0,0 +1,24 @@
+// ForceArrow.cs
+using UnityEngine;
+
+public class ForceArrow : MonoBehaviour
+{
+    [SerializeField] private float maxForceFactor = 5f; // 控制箭头长度的参数
+    [SerializeField] private float destroyDelay = 2f; // 箭头消失延迟
+    [SerializeField] private float maxScale = 0.8f;
+
+    private Vector3 forceDirection;
+    private float forceMagnitude;
+
+    public void Initialize(Vector3 force)
+    {
+        forceDirection = force.normalized;
+        forceMagnitude = force.magnitude;
+
+        // 调整箭头方向与长度
+        transform.rotation = Quaternion.LookRotation(forceDirection);
+        float scaleFactor = Mathf.Clamp(forceMagnitude / maxForceFactor, 0.46f, maxScale);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, scaleFactor);
+
+    }
+}
